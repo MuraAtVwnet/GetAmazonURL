96ac3aa9c914c01cecd8ec4ea3a6804761026d93
Syuichi Murashima <mura@vwnet.jp>
2023-09-20 06:41:38 +0900

    サクラチェッカー でチェックするオプション追加

--- GetAmazonURL.psm1
+++ GetAmazonURL.psm1
@@ -1,26 +1,53 @@
 ﻿############################################################
-# Reject Amazon URL
+# Reject Amazon URL noise
 ############################################################
-function GetAmazonURL(){
+function GetAmazonURL([switch]$Check){
+	$ChromePath = Join-Path $env:ProgramFiles "Google\Chrome\Application\chrome.exe"
+	$SakuraURL = "https://sakura-checker.jp/search/"
+
 	$FullURL = Get-Clipboard
-	if( $FullURL.Contains("/product/") ){
-		$dummy = $FullURL -match "/product/(?<DPUrl>.+?)/"
-		if($dummy -eq $false ){
-			$dummy = $FullURL -match "/product/(?<DPUrl>.+?)$"
-		}
+	if( ($FullURL -match "(?<GoodsUrl>https://www`.amazon`.co`.jp/dp/.+/?)") -or ($FullURL -match "(?<GoodsUrl>https://www`.amazon`.com/dp/.+/?)")){
+		$GoodsUrl =  $Matches.GoodsUrl
+		$dummy = $FullURL -match "/dp/(?<DPUrl>.+?)$"
+		$DP = $Matches.DPUrl
 	}
 	else{
-		$dummy = $FullURL -match "/dp/(?<DPUrl>.+?)/"
-		if($dummy -eq $false ){
-			$dummy = $FullURL -match "/dp/(?<DPUrl>.+?)$"
+		if( $FullURL.Contains("/product/") ){
+			$dummy = $FullURL -match "/product/(?<DPUrl>.+?)/"
+			if($dummy -eq $false ){
+				$dummy = $FullURL -match "/product/(?<DPUrl>.+?)$"
+			}
 		}
+		else{
+			$dummy = $FullURL -match "/dp/(?<DPUrl>.+?)/"
+			if($dummy -eq $false ){
+				$dummy = $FullURL -match "/dp/(?<DPUrl>.+?)$"
+			}
+		}
+
+		$DP = $Matches.DPUrl
+		$DPUrl = "/dp/" + $DP
+
+		$dummy = $FullURL -match "(?<AmazonUrl>https://.+?)/"
+		$AmazonUrl = $Matches.AmazonUrl
+		$GoodsUrl = $AmazonUrl + $DPUrl
 	}
 
-	$DPUrl = "/dp/" + $Matches.DPUrl
-
-	$dummy = $FullURL -match "(?<AmazonUrl>https://.+?)/"
-	$AmazonUrl = $Matches.AmazonUrl
-	$GoodsUrl = $AmazonUrl + $DPUrl
 	echo $GoodsUrl
 	$GoodsUrl | Set-Clipboard
+
+	if($Check){
+		if( Test-Path $ChromePath ){
+			if( $GoodsUrl -match "`.com" ){
+				echo "Sakura Checker is only supported by Amazon Japan"
+			}
+			else{
+				$ProdactSearchURL = $SakuraURL + $DP + "/"
+				. $ChromePath $ProdactSearchURL
+			}
+		}
+		else{
+			echo "Sakura Checker requires Google Chrome"
+		}
+	}
 }
