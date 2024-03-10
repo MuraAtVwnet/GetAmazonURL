01ecf2127f576d6b42e2445f93d21fd4378b9b37
Syuichi Murashima <mura@vwnet.jp>
2024-03-10 13:20:06 +0900

    互換性確保

--- install.ps1
+++ install.ps1
@@ -2,14 +2,18 @@
 $ModuleName = "GetAmazonURL"
 
 # Module Path
+if(($PSVersionTable.Platform -eq "Win32NT") -or ($PSVersionTable.Platform -eq $null)){
 $ModulePath = Join-Path (Split-Path $PROFILE -Parent) "Modules"
+}else{
+$ModulePath = Join-Path ($env:HOME) "/.local/share/powershell/Modules"}
+
 $NewPath = Join-Path $ModulePath $ModuleName
 
 # Make Directory
 if( -not (Test-Path $NewPath)){
-	md $NewPath
+	New-Item $NewPath -ItemType Directory -ErrorAction SilentlyContinue
 }
 
 # Copy Module
 $ModuleFileName = Join-Path $PSScriptRoot ($ModuleName + ".psm1")
-copy $ModuleFileName $NewPath
+Copy-Item $ModuleFileName $NewPath
--- uninstall.ps1
+++ uninstall.ps1
@@ -2,7 +2,10 @@
 $ModuleName = "GetAmazonURL"
 
 # Module Path
+if(($PSVersionTable.Platform -eq "Win32NT") -or ($PSVersionTable.Platform -eq $null)){
 $ModulePath = Join-Path (Split-Path $PROFILE -Parent) "Modules"
+}else{
+$ModulePath = Join-Path ($env:HOME) "/.local/share/powershell/Modules"}
 $RemovePath = Join-Path $ModulePath $ModuleName
 
 # Remove Direcory
